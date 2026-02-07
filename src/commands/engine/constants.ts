8ea23a329da02639274ffb9a38dc546ae58d20b8
Rioh <90512887+Rioh1118@users.noreply.github.com>
2026-02-07 11:33:37 +0900

    定数 enginesディレクトリを導入

--- src/commands/engine/constants.ts
+++ src/commands/engine/constants.ts
@@ -1,6 +1,6 @@
 export const ENGINE_CONSTANTS = {
   ENGINE_PATH:
-    "/Users/riohatta/test_shogi_engine/li/YaneuraOu_NNUE_halfKP1024X2_8_32-V830Git_APPLEM1",
+    "/Users/riohatta/test_shogi_engine/engines/YaneuraOu_NNUE_halfKP1024X2_8_32-V830Git_APPLEM1",
   WORK_DIR: "/Users/riohatta/test_shogi_engine/li",
   EVAL_DIR: "/Users/riohatta/test_shogi_engine/li/eval",
   BOOK_DIR: "/Users/riohatta/test_shogi_engine/li/book",
